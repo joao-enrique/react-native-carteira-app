dc7e8f68fd7ec89cda7498630dee703c84db17d1
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-10-03 12:53:06 -0300

    💾 Feat: add GET summary transaction route

--- backend/server.js
+++ backend/server.js
@@ -96,6 +96,31 @@ app.delete("/api/transactions/:id", async(req,res) => {
     }
 })
 
+app.get("/api/transactions/summary/:userId", async(req,res) => {
+    try {
+        const {userId} = req.params;
+
+        const balanceResult = await sql`
+            SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
+        `
+        const incomeResult = await sql`
+            SELECT COALESCE(SUM(amount), 0) as income FROM transactions WHERE user_id = ${userId} AND amount > 0
+        `
+        const expenseResult = await sql`
+            SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions WHERE user_id = ${userId} AND amount < 0
+        `
+
+        res.status(200).json({
+            balance: balanceResult[0].balance,
+            income: incomeResult[0].income,
+            expenses: expenseResult[0].expenses,
+        })
+    } catch (error) {
+        console.log("Error gettin summary", error);
+        res.status(500).json({message: "Internal server error"})
+    }
+})
+
 app.get("/", (req, res) => {
     res.send("It's working 123")
 })
