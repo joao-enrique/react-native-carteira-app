0e2b05438de28e08ebe679d8e8ed356354274497
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-10-04 09:26:37 -0300

    💾 Feat: fetched transaction use hook in API

--- mobile/app/(root)/index.tsx
+++ mobile/app/(root)/index.tsx
@@ -2,9 +2,18 @@ import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 import { SignOutButton } from '@/components/SignOutButton'
+import { useTransactions } from "../../hooks/useTransactions.js"
+import { useEffect } from 'react'
 
 export default function Page() {
   const { user } = useUser()
+  const {transactions, summary, isLoading, loadData, deleteTransaction} = useTransactions(user.id)
+
+  useEffect(() => {
+    loadData();
+  }, [loadData])
+  console.log("data is loaded", transactions)
+  console.log("summary", summary)
 
   return (
     <View>
